e92f9db1a3d8b210b67c589ae233f971c783b752
Shamiun Basir <slshohag0528@gmail.com>
2020-03-15 03:24:56 +0600

    1st commit

---
+++ Class1/NewFolder/DST.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class1.NewFolder
+{
+    class DST
+    {
+    }
+}
---
+++ Class1/Program.cs
@@ -0,0 +1,15 @@
+﻿using Class1.NewFolder;
+using System;
+
+namespace Class1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DST d = new DST();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
